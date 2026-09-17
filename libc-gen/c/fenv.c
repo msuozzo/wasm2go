652e0134eb7281ceb6cde68b92d7256da3530d32
Nuno Cruces <ncruces@users.noreply.github.com>
2026-09-17 16:10:41 +0100

    Float env

--- libc-gen/c/fenv.c
+++ libc-gen/c/fenv.c
@@ -1,8 +1,16 @@
 #include <fenv.h>
 
 int fegetround(void) { return FE_TONEAREST; }
-int fesetround(int r) { return 0; }
+int fesetround(int round) { return 0; }
 
-int feclearexcept(int mask) { return mask; }
-int feraiseexcept(int mask) { return mask; }
+int feclearexcept(int mask) { return 0; }
+int feraiseexcept(int mask) { return 0; }
 int fetestexcept(int mask) { return 0; }
+
+int fegetenv(fenv_t* envp) { return 0; }
+int feholdexcept(fenv_t* envp) { return 0; }
+int fesetenv(const fenv_t* envp) { return 0; }
+int feupdateenv(const fenv_t* envp) { return 0; }
+
+int fegetexceptflag(fexcept_t* fp, int mask) { return 0; }
+int fesetexceptflag(const fexcept_t* fp, int mask) { return 0; }
--- libc-gen/c/fenv.h
+++ libc-gen/c/fenv.h
@@ -7,6 +7,21 @@ int fesetround(int);
 
 #define FE_ALL_EXCEPT 0
 
+typedef unsigned long fexcept_t;
+
 int feclearexcept(int);
 int feraiseexcept(int);
 int fetestexcept(int);
+int fegetexceptflag(fexcept_t*, int);
+int fesetexceptflag(const fexcept_t*, int);
+
+typedef struct {
+  unsigned long __cw;
+} fenv_t;
+
+#define FE_DFL_ENV ((const fenv_t*)-1)
+
+int fegetenv(fenv_t*);
+int feholdexcept(fenv_t*);
+int fesetenv(const fenv_t*);
+int feupdateenv(const fenv_t*);
